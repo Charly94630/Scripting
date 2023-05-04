e46417a15809b53d795924ac52a0e8f7ec032410
Carlos Adrián López <97137104+Charly94630@users.noreply.github.com>
2023-05-04 20:59:33 -0300

    Add files via upload

---
+++ DNS_settings.ps1
@@ -0,0 +1,17 @@
+﻿Function Set-DNS 
+{
+    $currentDNSservers = (Get-WmiObject Win32_NetworkAdapterConfiguration -Filter "IPEnabled='True'").DNSServerSearchOrder
+    $DNSservers = "172.21.10.52", "172.18.10.51"
+    if ((Compare-Object -ReferenceObject $currentDNSservers -DifferenceObject $DNSservers).Length -eq 0) 
+    {
+     exit
+    }
+    else 
+    {
+      $adapter = Get-WmiObject Win32_NetworkAdapterConfiguration -Filter "IPEnabled='True'"
+      $adapter.SetDNSServerSearchOrder($DNSservers) 
+      Add-Content -Path "C:\temp\DNS_changelog.txt" -Value "$env:computerName DNS changed"
+    }
+}
+
+Set-DNS
