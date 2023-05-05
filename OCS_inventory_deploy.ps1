0611f09620ece88ea0ade396679d1dd80e6a2aec
Carlos Adrián López <97137104+Charly94630@users.noreply.github.com>
2023-05-04 21:02:38 -0300

    Add files via upload

---
+++ OCS_inventory_deploy.ps1
@@ -0,0 +1,25 @@
+﻿$USER_PATH = "C:\temp"
+$UNC_PATH = "\\tata.com.uy\archivos\publicas\OPRTIT\OCS"
+
+
+
+function Check_OCS {
+
+return (Get-ChildItem "HKLM:Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
+  
+Where-Object { $_."Name" -like "*$OCS*" } 
+
+
+
+}
+
+if($null = (Check_OCS)) {
+
+   
+  
+  copy-item -path $UNC_PATH -Destination $USER_PATH -Recurse
+  Start-Process "C:\temp\OCS\OcsPackage64.exe"  -Wait -ArgumentList "/S /silent /quiet" -PassThru
+
+
+
+}
