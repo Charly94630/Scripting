23db10ee5a0f6ece84244ec10e69230bfebbf2be
Carlos Adrián López <97137104+Charly94630@users.noreply.github.com>
2023-05-04 21:02:09 -0300

    Add files via upload

---
+++ JavaRPM.ps1
@@ -0,0 +1,22 @@
+﻿$File = "C:\temp\JavaRPM\JavaSetup8u341.exe"
+$java = Get-WmiObject -Class win32_product | Where-Object {$_.name -like '*Java 8*'} | Select-Object -First 1 
+$currentVersion = [version]$app.Version
+
+if ($java -and ($java.version -ne "8.0.3410.10"))  {
+
+$java.uninstall()
+Start-Process -FilePath $File -ArgumentList "/s" -Wait
+ } 
+
+
+elseif (-not($java)) {
+Start-Process -FilePath $File -ArgumentList "/s" -Wait
+
+}
+
+else {$java.version -ge "8.0.3410.10" 
+
+Exit
+
+}
+
