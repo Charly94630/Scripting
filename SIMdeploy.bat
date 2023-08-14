862cc452df7a53e16b2a85634a5185596add24e6
Carlos Adrián López <97137104+Charly94630@users.noreply.github.com>
2023-08-14 20:36:16 -0300

    Update SIMdeploy.bat

--- SIMdeploy.bat
+++ SIMdeploy.bat
@@ -4,12 +4,12 @@ set /p ip=Ingrese una Ip?:
 net use \\%ip%\c$ /user:administrador T4t4nk4m0n!2020
 mkdir "\\%ip%\c$\SIM"
 echo Copiando Archivos
-robocopy "\\172.19.92.133\share\SIM " "\\%ip%\c$\SIM " /E
+robocopy "\\xxx.xx.xx.xx\xx\xx " "\\%ip%\c$\SIM " /E
 
 
 
 
-wmic /user:administrador /password:T4t4nk4m0n!2020 /node:%ip% process call create "cmd.exe /c start /WAIT C:\SIM\SIM.bat"
+wmic /user:xxx /password:xxx /node:%ip% process call create "cmd.exe /c start /WAIT C:\SIM\SIM.bat"
 net use \\%ip%\c$ /d /y>nul 2>&1
 
 pause
