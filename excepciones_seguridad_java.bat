70144fa96a684195e88d83dc606aadd2e7ec6156
Carlos Adrián López <97137104+Charly94630@users.noreply.github.com>
2023-08-14 20:41:29 -0300

    Update excepciones_seguridad_java.bat

--- excepciones_seguridad_java.bat
+++ excepciones_seguridad_java.bat
@@ -8,24 +8,14 @@ cd  /D %userprofile%\AppData\LocalLow\Sun\Java\Deployment\security
 
 @echo off
 
-echo http://10.197.48.184:16003 >> exception.sites
-echo http://orm1601:16003 >> exception.sites
-echo http://sit1r3-mom-app01:11003/ >> exception.sites
-echo http://sit1r3-rpm-app01.or.tata.com.uy:11003/ >> exception.sites
-echo http://sit1r3-rpm-app01:11003/ >> exception.sites
-echo http://sit1r3-sim-app01.or.tata.com.uy:7003 >> exception.sites
-echo http://uatata-mom-app01:11003 >> exception.sites
-echo http://uatata-rpm-app01.or.tata.com.uy >> exception.sites
-echo http://uatata-rpm-app01.or.tata.com.uy:11003/ >> exception.sites
-echo http://uatata-rpm-app01:11003 >> exception.sites
-echo https://prd-rpm-app01.or.tata.com.uy:11003 >> exception.sites
-echo https://prd-rpm-app01.or.tata.com.uy:11004 >> exception.sites
-echo http://uatata-rpm-app01:11003 >> exception.sites
-echo http://uatata-mom-app01:11003 >> exception.sites
-echo https://prd-rpm-app01.or.tata.com.uy:11004 >> exception.sites
-echo http://prd-rpm-app01.or.tata.com.uy:11003 >> exception.sites
-echo https://prd-rms-app01.or.tata.com.uy:7004 >> exception.sites
-echo http://prd-rms-app01.or.tata.com.uy:7003 >> exception.sites
+echo http://xx.xx.xx.xx:xx >> exception.sites
+echo http://xx:xx >> exception.sites
+echo http://xx-xx-xx:xx/ >> exception.sites
+echo http://xx-xx-xx.xx.xx.xx.xx:/ >> exception.sites
+echo http://xx-xx-xx:xx/ >> exception.sites
+echo http://xx-xx-xx.or.xx.xx.xx: >> exception.sites
+echo http://xx-xx-xx.or.xx.xx.xx: >> exception.sites
+
 
 @echo Excepciones cargadas correctamente.
 
