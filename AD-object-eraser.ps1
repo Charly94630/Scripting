1188444b5426b236c8a0e5b955a7e91b94d8f65a
Carlos Adrián López <97137104+Charly94630@users.noreply.github.com>
2023-10-05 20:28:25 -0300

    Add files via upload

---
+++ AD-object-eraser.ps1
@@ -0,0 +1,27 @@
+﻿$Intro = @' 
+
+    ***********************                               ***********************
+
+
+                                 AD Object Eraser  
+   
+    ***********************                               ***********************
+'@
+  
+  
+write-host -ForegroundColor Magenta $Intro
+
+$pc = ""
+while (!$computer) {
+    try {
+        $pcs = Read-Host -Prompt 'Equipo a borrar'
+        $computer = Get-ADComputer -Identity $pc
+        Remove-ADComputer -Identity $pc -Confirm:$false
+        Write-Host "Equipo $pc eliminado correctamente"
+    }
+    catch {
+        Write-Host "Error: $($_.Exception.Message)"
+        Write-Host "El equipo ingresado no es válido"
+        $computer = $null
+    }
+}
---
+++ drivers_download.ps1
@@ -0,0 +1,19 @@
+﻿Function GithubAPI() {$githubUsername = "carloslopez-tata"
+$githubRepo = "PriterDrivers"
+$pat = "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"
+$outputPath = "C:\temp\drivers.zip"
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($githubUsername):$($pat)"))
+$archiveUrl = "https://api.github.com/repos/$githubUsername/$githubRepo/zipball/main"
+}
+
+GithubAPI
+if (-not(Test-Path -Path "C:\Windows\System32\DriverStore\FileRepository\oemsetup.inf_amd64_c7674953e75f1cca")){
+Invoke-RestMethod -Uri $archiveUrl -Headers @{ Authorization = "Basic $base64AuthInfo" } -OutFile $outputPath
+Expand-Archive -Path $outputPath -DestinationPath "c:\temp"
+
+}
+
+
+
+
+
